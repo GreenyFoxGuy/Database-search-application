5fc6b9d5d903799757c107288799348a530c66a7
JKolenak <greenyfoxguy@gmail.com>
2026-06-10 14:50:36 +0200

    Added means and sums of population, age and men/women

--- postgreSQL.py
+++ postgreSQL.py
@@ -33,9 +33,24 @@ def search_in_db(show_options, option):
 
     elif show_options == "N":
         user_input = input("Zadejte kód okresu: ")
-        query = 'SELECT okresy.nazev, obce_pob.nazev, obce_pob.pocet_obyvatel, obce_pob.prumerny_vek FROM okresy JOIN obce_pob ON obce_pob.id_okres = okresy.id_okres WHERE okresy.id_okres = %s'
+        query = """
+                SELECT
+                    okresy.nazev AS okres,
+                    obce_pob.nazev AS obec,
+                    obce_pob.pocet_obyvatel,
+                    obce_pob.prumerny_vek,
+                    SUM(obce_pob.pocet_obyvatel) OVER () AS okres_obyvatel,
+                    AVG(obce_pob.prumerny_vek) OVER () AS okres_prumerny_vek,
+                    SUM(obce_pob.pocet_muzi) OVER () AS okres_muzi,
+                    SUM(obce_pob.pocet_zeny) OVER () AS okres_zeny
+                FROM okresy
+                JOIN obce_pob
+                    ON obce_pob.id_okres = okresy.id_okres
+                WHERE okresy.id_okres = %s;
+                """
         cur.execute(query, (user_input,))
         rows = cur.fetchall()
+        print(rows[0])
         town = ""
 
     return rows, town
@@ -49,18 +64,24 @@ def reformate_data(data, option):
     reformated_data = []
     if option == "2":
         district = data[0][0]
+        sum_population = data[0][4]
+        mean_age = data[0][5]
+        sum_men = data[0][6]
+        sum_women = data[0][7]
         for row in data:
             reformated_row = row[1] + " | " + str(row[2]) + " | " + str(row[3])
             reformated_data.append(reformated_row)
+
+            return reformated_data, district, sum_population, mean_age, sum_men, sum_women
+
     if option == "1":
         for town in data:
             reformated_data.append(town[0])
-        district = ""
 
-    return reformated_data, district
+        return reformated_data, "", "", "", "", ""
 
 
-def print_data(data, district, option, town):
+def print_data(data, district, option, town, sum_population=None, mean_age=None, sum_men=None, sum_women=None):
 
     if option == "1":
         print(f"Vyhledávání pro {town}:")
@@ -70,6 +91,11 @@ def print_data(data, district, option, town):
     for row in data:
         print(row)
     print("-"*15)
+
+    if option == "2":
+        print("Total population: ", sum_population)
+        print("Mean age: ", mean_age)
+        print("Men to women ration: ", sum_men, "/", sum_women)
     return 0
 
 def main():
@@ -83,18 +109,18 @@ def main():
         return True
     elif option == "2":
         show_options = input("Zobrazit okresy? [A/N]:")
-        if show_options != "A" or show_options != "N":
+        if show_options != "A" and show_options != "N":
             print("Please choose a valid option!")
             return False
         data, town = search_in_db(show_options, option)
         if show_options == "A":
             district = ""
         else:
-            data, district = reformate_data(data, option)
-        print_data(data, district, option, town)
+            data, district, sum_population, mean_age, sum_men, sum_women = reformate_data(data, option)
+        print_data(data, district, option, town, sum_population, mean_age, sum_men, sum_women)
     elif option == "1":
         data, town = search_in_db("N", option)
-        data, district = reformate_data(data, option)
+        data, district, n, o, p, e = reformate_data(data, option)
         print_data(data, "", option, town)
 
     return False
