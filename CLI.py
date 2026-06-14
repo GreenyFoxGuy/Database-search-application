63cbd1f6d6115a6524b7b1e4a89681380d6b123e
GreenyFoxGuy <greenyfoxguy@gmail.com>
2026-06-14 09:40:47 +0200

    Finished CLI Application

--- CLI.py
+++ CLI.py
@@ -1,17 +1,69 @@
 from Database import Database
 
-
 def print_menu():
-    op_find_town, op_towns_in_district, op_district_list, op_district_stats, op_end = 1, 2, 3, 4, 0
+    op_find_town, op_towns_in_district, op_list_districts, op_district_information, op_end = 1, 2, 3, 4, 0
     user_options = ["1 - Hledat obec",
                     "2 - Obce v okrese",
                     "3 - Seznam okresů",
                     "4 - Statistiky okresu",
                     "0 - Ukončit",]
-    print("="*20+"\nDemografie ČR\n"+"="*20)
+    print("="*20+"\nDemografie ČR\n"+"="*20+"\n")
     print("\n".join(user_options))
 
+def print_result(data):
+
+    for row in data:
+        print(row)
+
+def find_town():
+
+    phrase_to_search = input("Hledat obec: ")
+    print(f"Vyhledávání pro {phrase_to_search}:")
+
+    try:
+        data = Database().search_town(phrase_to_search)
+    except IndexError:
+        print("Nebyla nalezena žádná obec.")
+        continue
+    
+    print_result(data)
+
+def towns_in_district():
+
+    district_id = input("Zadejte ID okresu: ")
+
+    try:
+        data = Database().get_district_info(district_id)
+    except IndexError:
+        print("Nebyl nalezen žádný okres.")
+        continue
+
+    print_result(data)
+
+def list_districts():
+
+    try:
+        data = Database().get_districts()
+    except IndexError:
+        print("Nebyl nalezen žádný okres.")
+        continue
+
+    print_result(data)
+
+def district_information():
+
+    district_id = input("Zadejte ID okresu: ")
+
+    try:
+        data = Database().get_district_mean_info(district_id)
+    except IndexError:
+        print("Nebyl nalezen žádný okres.")
+        continue
+
+    print_result(data)
+
 def main():
+
     while True:
         print_menu()
         try:
@@ -24,16 +76,19 @@ def main():
             continue
 
         if chosen_option == op_find_town:
-            phrase_to_search = input("Hledat obec: ")
-            print(f"Vyhledávání pro {phrase_to_search}:")
-            try:
-                data = Database().search_town(phrase_to_search)
-            except IndexError:
-                print("Nebyla nalezena žádná obec.")
-                continue
+            find_town()
 
-            for town in data:
-                print(town)
+        if chosen_option == op_towns_in_district:
+            towns_in_district()
+
+        if chosen_option == op_list_districts:
+            list_districts()
+
+        if chosen_option == op_district_information:
+            district_information()
+
+        if chosen_option == op_end:
+            break
 
 if __name__ == '__main__':
     main()
